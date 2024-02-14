e909c1f387ca789b23c50fc6d97a21d3bfa85540
Valeriia Zolotykh <98551132+ValeryZolotykh@users.noreply.github.com>
2024-02-14 12:46:32 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Board from "./pages/Board/components/Board/Board";
+import Home from "./pages/Home/Home";
+import { ToastContainer } from "react-toastify";
+import ModalWindow from "./components/modal-window";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ToastContainer
+        position="bottom-right" // Позиция уведомлений
+        autoClose={2500} // Время, через которое уведомление автоматически закроется (в миллисекундах)
+        theme={"dark"} // Тема уведомлений
+      />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="board/:board_id" element={<Board />}>
+          <Route path="card/:card_id" element={<ModalWindow />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
