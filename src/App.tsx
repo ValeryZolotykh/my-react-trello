e11d368ea94ac9b03d25472d9521505a87e6ad35
Valeriia Zolotykh <98551132+ValeryZolotykh@users.noreply.github.com>
2024-02-16 12:25:52 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,10 @@ function App() {
         theme={"dark"} // Тема уведомлений
       />
       <Routes>
-        <Route path="/my-react-trello" element={<Home />} >
-          <Route path="board/:board_id" element={<Board />}>
+        <Route path="/my-react-trello" element={<Home />} />
+          <Route path="/my-react-trello/board/:board_id" element={<Board />}>
             <Route path="card/:card_id" element={<ModalWindow />} />
           </Route>
-        </Route>
       </Routes>
     </Router>
   );
