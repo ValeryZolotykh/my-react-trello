9801beb82e6e528068e251036c47f638ff702685
Valeriia Zolotykh <98551132+ValeryZolotykh@users.noreply.github.com>
2024-02-16 11:51:27 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Board from "./pages/Board/components/Board/Board";
 import Home from "./pages/Home/Home";
 import { ToastContainer } from "react-toastify";
@@ -8,14 +8,14 @@ import ModalWindow from "./components/modal-window";
 
 function App() {
   return (
-    <Router basename="/my-react-trello">
+    <Router>
       <ToastContainer
         position="bottom-right" // Позиция уведомлений
         autoClose={2500} // Время, через которое уведомление автоматически закроется (в миллисекундах)
         theme={"dark"} // Тема уведомлений
       />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/my-react-trello" element={<Home />} />
         <Route path="board/:board_id" element={<Board />}>
           <Route path="card/:card_id" element={<ModalWindow />} />
         </Route>
