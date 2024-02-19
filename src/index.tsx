e749670e801a3709d63b377f6d71a151834a1dac
Valeriia Zolotykh <98551132+ValeryZolotykh@users.noreply.github.com>
2024-02-19 17:30:06 +0200

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store/store";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
 
-root.render(
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//         <App />
+//     </Provider>
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
         <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 reportWebVitals();
