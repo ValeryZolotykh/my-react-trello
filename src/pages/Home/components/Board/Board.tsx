38a41ad766f89bff5433dc2f90c0682ab4755245
Valeriia Zolotykh <98551132+ValeryZolotykh@users.noreply.github.com>
2024-02-19 17:09:03 +0200

    commit

--- src/pages/Home/components/Board/Board.tsx
+++ src/pages/Home/components/Board/Board.tsx
@@ -3,7 +3,7 @@ import './board.css'
 
 export default function Board({id, title, background}: { id:number; title: string, background:string}) {
   return (
-      <Link to={`/board/${id}`} className="board" style={{backgroundColor: background}}>
+      <Link to={`/my-react-trello/board/${id}`} className="board" style={{backgroundColor: background}}>
           <div className="title board__title">{title}</div>
       </Link>
     );
